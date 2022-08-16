6c18a1406e063cb0ca74457c79c5874e44e16785
Fares AbuIram <aburamf@gmail.com>
2022-08-16 21:27:06 +0300

    add get route

--- backend/controllers/secret.go
+++ backend/controllers/secret.go
@@ -3,7 +3,6 @@ package controllers
 import (
 	// "encoding/json"
 	"fmt"
-	"log"
 	"net/http"
 	"os"
 	"time"
@@ -14,7 +13,7 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/golang-jwt/jwt"
 	"go.mongodb.org/mongo-driver/bson"
-	// "go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 func GenerateToken(context *gin.Context) {
@@ -24,13 +23,16 @@ func GenerateToken(context *gin.Context) {
 		context.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"status": "error", "message": "Error on create secret.", "data": err})
 		return
 	}
+	// add object to database
 	result, err := database.SecretsCollection.InsertOne(database.Ctx, bson.D{
 		{Key: "expire", Value: input.Expire},
 		{Key: "views", Value: input.Views},
 	})
+	if err != nil {
+		fmt.Println(err)
+	}
 	var mySigningKey = []byte(os.Getenv("SECRET_TOKEN"))
-	expirationTime := time.Now().Add(time.Hour * time.Duration(input.Expire))
-	fmt.Println(expirationTime)
+	expirationTime := time.Now().Add(time.Microsecond * time.Duration(input.Expire))
 	token := jwt.New(jwt.SigningMethodHS256)
 	claims := token.Claims.(jwt.MapClaims)
 	claims["id"] = result.InsertedID
@@ -57,10 +59,46 @@ func GetToken(context *gin.Context) {
 	})
 
 	if err != nil {
-		log.Fatal(err)
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Invalid input"})
+		return
 	}
 
 	info := token.Claims.(*jwt.MapClaims)
-	fmt.Println("info: ", (*info)["id"])
-	context.JSON(http.StatusOK, gin.H{"data": token.Claims})
+	id := (*info)["id"]
+	objectId, err := primitive.ObjectIDFromHex(fmt.Sprintf("%v", id))
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Invalid input"})
+		return
+	}
+	// get the token object from database
+	var object models.Secret
+	err = database.SecretsCollection.
+		FindOne(database.Ctx, bson.D{{Key: "_id", Value: objectId}}).
+		Decode(&object)
+
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Invalid input"})
+		return
+	}
+
+	if object.Views <= 0 {
+		// delete expired object
+		_, err := database.SecretsCollection.DeleteOne(database.Ctx, bson.D{{Key: "_id", Value: objectId}})
+		if err != nil {
+			context.JSON(http.StatusInternalServerError, gin.H{"message": "something get wrong"})
+			return
+		}
+		context.JSON(http.StatusBadRequest, gin.H{"message": "No views available"})
+		return
+	}
+	// update the number of views
+	filter := bson.D{{Key: "_id", Value: objectId}}
+	update := bson.D{{Key: "$set", Value: bson.D{{Key: "views", Value: object.Views - 1}}}}
+	_, err = database.SecretsCollection.UpdateOne(database.Ctx, filter, update)
+
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "something get wrong"})
+		return
+	}
+	context.JSON(http.StatusOK, gin.H{"data": object})
 }
